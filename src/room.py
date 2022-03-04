7b20d2114e78000088da6e5b63901f5d9f23782d
Matt-Matthaiou <mat.matthaiou@gmail.com>
2022-03-04 19:07:40 +0000

    added Room class

---
+++ src/room.py
@@ -0,0 +1,5 @@
+class Room:
+
+    def __init__(self, capacity, song_list):
+        self.capacity = capacity
+        self.song_list = song_list
--- tests/room_test.py
+++ tests/room_test.py
@@ -4,4 +4,12 @@ from src.room import Room
 class TestRoom(unittest.TestCase):
 
     def setUp(self):
-        self.room = Room()
+        self.room = Room(5, ["Ace's high", "Master of puppets", "Azrael"])
+
+    def test_room_has_capacity(self):
+        self.assertEqual(5, self.room.capacity)
+
+    def  test_room_has_songs(self):
+        self.assertEqual("Ace's high", self.room.song_list[0])
+        self.assertEqual("Master of puppets", self.room.song_list[1])
+        self.assertEqual("Azrael", self.room.song_list[2])
