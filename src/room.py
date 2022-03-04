6dc43a57bc38ddee96458fbdf05fc9eb72506a63
Matt-Matthaiou <mat.matthaiou@gmail.com>
2022-03-04 19:36:40 +0000

    added function to checkout guests

--- src/room.py
+++ src/room.py
@@ -3,8 +3,11 @@ class Room:
     def __init__(self, capacity, song_list):
         self.capacity = capacity
         self.song_list = song_list
-        current_guests = []
+        self.current_guests = []
 
     def check_guest_in_room(self, guest):
-        self.currect_guests.append(guest)
+        self.current_guests.append(guest)
+
+    def checkout_guest(self, guest):
+        self.current_guests.remove(guest)
         
--- tests/room_test.py
+++ tests/room_test.py
@@ -10,7 +10,8 @@ class TestRoom(unittest.TestCase):
         self.song2 = Song("Master of puppets", 5.23)
         self.song3 = Song("Azrael", 6.10)
         self.room = Room(5, [self.song1, self.song2, self.song3])
-        self.guest = Guest("Matt", 35, 50, Song("Ace's high", 4.12))
+        self.guest1 = Guest("Matt", 35, 50, Song("Ace's high", 4.12))
+        self.guest2 = Guest("Lemmy", 60, 100, Song("Rock n roll", 2.57))
 
 
     def test_room_has_capacity(self):
@@ -22,5 +23,11 @@ class TestRoom(unittest.TestCase):
         self.assertEqual(self.song3, self.room.song_list[2])
     
     def test_check_guest_in_room(self):
-        self.room.check_guest_in_room(self.guest)
-        self.assertEqual(self.guest, self.room.current_guests[0])
+        self.room.check_guest_in_room(self.guest1)
+        self.assertEqual(self.guest1, self.room.current_guests[0])
+
+    def test_checkout_guest_from_room(self):
+        self.room.check_guest_in_room(self.guest1)
+        self.room.check_guest_in_room(self.guest2)
+        self.room.checkout_guest(self.guest1)
+        self.assertEqual(self.guest2, self.room.current_guests[0])
