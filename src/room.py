e199a91568ec895e82d59cd07bf73631ebd2dc85
Matt-Matthaiou <mat.matthaiou@gmail.com>
2022-03-04 19:28:04 +0000

    added function to check guests in room

--- src/room.py
+++ src/room.py
@@ -3,3 +3,8 @@ class Room:
     def __init__(self, capacity, song_list):
         self.capacity = capacity
         self.song_list = song_list
+        current_guests = []
+
+    def check_guest_in_room(self, guest):
+        self.currect_guests.append(guest)
+        
